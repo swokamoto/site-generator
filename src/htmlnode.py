c2140acc741ee1a2bf15ea4d3fefc507b7a2c2a1
swokamoto <swokamoto@gmail.com>
2025-09-14 00:30:27 -0400

    troubleshooting css

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,18 +19,22 @@ class HTMLNode:
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
 
+
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
-        
+
     def to_html(self):
         if self.value is None:
-            raise ValueError("LeafNode must have a value")
+            raise ValueError("invalid HTML: no value")
         if self.tag is None:
             return self.value
-
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
-    
+
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
         super().__init__(tag, None, children, props)
@@ -47,4 +51,3 @@ class ParentNode(HTMLNode):
 
     def __repr__(self):
         return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
-
